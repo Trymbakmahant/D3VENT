3712669ebf110da9c7940d1afaf01201c42c15e1
6str <56482955+6str@users.noreply.github.com>
2022-09-07 12:17:58 +0100

    setting up with hardhat

--- contracts/contracts/d3vent.sol
+++ contracts/contracts/d3vent.sol
@@ -1,39 +1,3 @@
-    // todos and questions of what to do
-
-    // should we allow multiple organisers?
-    // should we allow an event wallet address so funds can be withdrawn to a non organiser address e.g. a multisig wallet?
-
-    // setting up a voting mechanism where joiners can vote to call the event failed in some way 
-    // and so prevent the organiser withdrawal, and can claim back their funds
-    // a period up to which a joiner can unjoin and get a refund. after a certain point it's too late
-    // this should be expressed as a period before event start time. prevet organiser simply bringing the event forward and 
-    // being able rug pull / be sure prevent dirty tricks. e.g. make sure unjoining can't be shortened below a certain period
-    // need proof of personhood otherwise susceptible sybil attack from both sides
-
-    // add support for worldcoin plus another wallet
-
-    // access control to allow end user owner only. trustless or also admins. are there any organisational admins?
-
-    // should created events be immutable or setters for most things so they can be changed. what should be ammendable.
-    // if changes allowed, should have 1 setter function with all changeable settings, or setter for each
-    // organiser might reasonablely need ot change a event date or change a URI.
-    // change: organiser, multiple organisers, event name, uri, datetime, price capacity
-    // allow set different address as organiser at create time or able to change after
-    // create event: consider capacity of zero is unlimited or throw error?
-    // being able to change the event date does present possibilities of gaming the system and if allowed there should be restrictions
-    // if organiser change change dates, joiners should be allowed to unjoin and get their funds back.
-    // workflow questions: should newly created even have eventActive as param or always true/false
-    // should there be a way to cancel an event - how should that be represented
-    // should we have a cancelled / finished status
-
-    // create event organiser always msg.sender? setOrgnaiser function to change?
-    // should event have a closed/finished dateTime?
-    // should joiner address always be msg.sender or allow to join another address?
-    
-    // don't think there's any need to zero out joiner balances after organiser withdrawal, but could add a flag for added safety
-    // would be too gassy iterating through a large number of joiners
-    // 
-    
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.7;
 
@@ -48,23 +12,21 @@ contract d3vent {
     event AdminAdded(address indexed newAdmin, address indexed addedBy);
     event AdminDeleted(address indexed deletedAdmin, address indexed deletedBy);
     event WorldcoinAddressChanged(address indexed newAddress, address indexed changedBy);
-
+    event UserVerified(address indexed user);
 
     using ByteHasher for bytes;
 
     /// @notice Thrown when attempting to reuse a nullifier
     error InvalidNullifier();
  
-
     /// @dev The World ID instance that will be used for verifying proofs
     IWorldID internal immutable worldId;
 
     /// @dev The World ID group ID (always 1)
     uint256 internal immutable groupId = 1;
 
-    // using ByteHasher for bytes;
-
     address private worldcoin;  //note: this is immutable in Worlcoin's example contract
+    
     /// @dev Whether a nullifier hash has been used already. Used to guarantee an action is only performed once by a single person
     mapping(uint256 => bool) internal nullifierHashes;
 
@@ -73,14 +35,6 @@ contract d3vent {
     uint immutable withdrawalBuffer;
     uint adminsCount;
     
-    
-    //uint immutable unjoinBuffer; // not implemented
-    
-    // not implemented
-    // struct user {
-    //     address userAddress;
-    //     event_[] eventIds;  // do we need quick lookups and/or able to iterate eventIds?
-    // }
 
     struct event_ {
         address organiser;
@@ -97,10 +51,11 @@ contract d3vent {
 
     event_[] events;
 
-    mapping(uint => mapping(address => bool)) isJoined;    // eventId => userAddr => isJoined
-    mapping(uint => mapping(address => uint)) joinerBalances;   // eventId => userAddr => balance
-    mapping(uint => uint) eventBalances;    // eventId => event balance
-    mapping(address => bool) isAdmin;   // admin address to bool
+    mapping(address => bool) public isVerified; // user address => bool
+    mapping(uint => mapping(address => bool)) public isJoined;    // eventId => user address => isJoined
+    mapping(uint => mapping(address => uint)) public joinerBalances;   // eventId => user address => balance
+    mapping(uint => uint) public eventBalances;    // eventId => event balance
+    mapping(address => bool) public isAdmin;   // admin address to bool
     
 
     constructor (uint _withdrawalBuffer, IWorldID _worldId) {
@@ -112,7 +67,7 @@ contract d3vent {
 
 
     function kill() external onlyAdmins {
-        selfdestruct(msg.sender);
+        selfdestruct(payable(msg.sender));
     }
 
 
@@ -151,9 +106,9 @@ contract d3vent {
 
         // Finally, execute your logic here, for example issue a token, NFT, etc...
         // Make sure to emit some kind of event afterwards!
-
+        isVerified[signal] = true;
+        emit UserVerified(signal);
     }
-
 
 
     modifier onlyOrganiser (uint _id) {
@@ -182,18 +137,14 @@ contract d3vent {
     }
 
 
-    // test parameters: "test event name","https://livepeer.org/123",1662994265,1000,1000000,false
     function createEvent(
-        //string calldata _eventName,
         string calldata _name,
         string calldata _uri,
         uint _dateTime,
         uint _price,
         uint128 _capacity,
         bool _isJoinable
-        ) external {              // public to allow internal calls for testing
-
-        // sanity checks
+        ) external {
         require(_dateTime > block.timestamp, "date/time in past");
 
         event_ memory newEvent;
@@ -213,7 +164,6 @@ contract d3vent {
 
 
     function setOrganiser(uint _id, address _newOrganiser) external onlyOrganiser(_id) {
-        // sanity checks
         require(_newOrganiser != address(0), "invalid: zero address");
         require(_id <= eventIds, "invalid event id");
         events[_id].organiser = _newOrganiser;
@@ -227,9 +177,7 @@ contract d3vent {
         emit JoinableSet(_id, _isJoinable);
     }
 
-    // @dev needs proof of personhood
-    // support superfluid payments? how would that affect logic
-    // 
+
     function joinEvent(uint _id) external payable {
         require(_id <= eventIds, "invalid event id");
         require(events[_id].isJoinable, "cant join at this time");
---
+++ contracts/scripts/constructorArgs.js
@@ -0,0 +1,15 @@
+// use an arguments.js file to pass constructor arguments 
+// when verifying a contract. can use in run and deploy scripts to keep one source of arge
+// example code below
+require("dotenv").config();
+
+const worldcoin_addr = process.env.WORLDID_ADDR
+console.log("worldcoin addr" + worldcoin_addr)
+
+const days = 60 * 60 * 24 * 1000
+const withdrawBuffer = 3 * days
+
+module.exports = [
+    withdrawBuffer,
+    worldcoin_addr,
+];
