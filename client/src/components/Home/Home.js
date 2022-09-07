bb0cc44a36966285f260732737f776577c519da9
kosmos22 <97914996+kosmos22@users.noreply.github.com>
2022-09-08 00:01:57 +0530

    H1 edited

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,10 +1,9 @@
 const Home = () => {
-    
-    return (
-        <div>
-            <h1>This is home</h1>
-        </div>
-    )
-};
+  return (
+    <div>
+      <h1>thisss is home</h1>
+    </div>
+  )
+}
 
-export default Home;
+export default Home
