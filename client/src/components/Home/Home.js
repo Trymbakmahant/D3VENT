e168b871b550466f2e7662bb8e09565721c49b6f
Trymbak Mahant <trymbak0102mahant@gmail.com>
2022-09-08 14:09:26 +0530

    worldID widget added

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,9 +1,11 @@
+import WORLDIDwidget from "../Home/WorldIDwidget";
 const Home = () => {
   return (
     <div>
+      <WORLDIDwidget />
       <h1>thisss is home</h1>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
