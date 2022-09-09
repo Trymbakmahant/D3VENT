ad697aff595671d3adf8c8437229a34364ea41af
6str <56482955+6str@users.noreply.github.com>
2022-09-09 20:37:30 +0100

    ignore me

---
+++ contracts/hardhat.config.js
@@ -0,0 +1,39 @@
+require("dotenv").config();
+//require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+require("hardhat-gas-reporter"); 
+require('hardhat-log-remover');
+require('solidity-coverage');
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.10",
+  settings: {
+    optimizer: {
+      enabled: false,
+      runs: 200,
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+  gasReporter: {
+    //enabled: (process.env.REPORT_GAS) ? true : false,
+    enabled: true,
+    token: "MATIC",
+    currency: 'USD',
+    gasPrice: 21
+    //excludeContracts: Migrations.sol
+  },
+  networks: {
+    ganache: {
+      url: "http://172.24.16.1:8545",
+      // accounts: [privateKey1, privateKey2, ...]
+    },
+    mumbai: {
+      url: process.env.STAGING_ALCHEMY_KEY,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+};
