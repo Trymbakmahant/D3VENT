7823edfc734d4123188f7fa38a6e64bbe0bb3e13
phill <56482955+6str@users.noreply.github.com>
2022-09-14 21:11:35 +0100

    recent changes (#37)
    * dep addr updated
    
    * deployed new contract
    added getAllEvents()
    added event duration to events
    removed worldcoin address as var not used
    
    * update contractAddress file to match format found in version conflict
    
    * removed capacity and price from event props
    added setPlaybackUri function

--- client/src/constants/contractAddress.js
+++ client/src/constants/contractAddress.js
@@ -1,3 +1,3 @@
-const addressofContract = '0x9527C37994532e2e40F79a0804cE496C20BF39F9';
-
+const addressofContract = '0x3cFd8D9A342D388aC6e904522028d640d90d7645'
 export default addressofContract;
+
