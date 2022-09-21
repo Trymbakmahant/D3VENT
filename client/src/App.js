eb4a2f5b79048c603f84aa6fe88f5a33c69000a5
Vaibhav Arora <aroravaibhav817@gmail.com>
2022-09-21 18:42:03 +0530

    added verified function

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,32 @@
 import Navbar from './components/Navbar/Navbar'
 import Paths from './Paths/Paths'
+import { useContext, useState } from 'react';
+import { AppContext } from './components/context/AddressContext';
 
+import WORLDIDwidget from "./components/Home/WorldIDwidget";
 function App() {
+  const [checkIsVerified, setCheckIsVerified] = useState(false);
+  const ctx = useContext(AppContext);
+
+  let getValue;
+  const getData = async () =>{
+
+    getValue = await ctx.sharedState.checkIsVerified();
+    setCheckIsVerified(getValue)
+  }
+
+  getData();
+
   return (
     <>
-      <Navbar />
-      <Paths />
+      {  checkIsVerified ?
+        <div>
+          <Navbar />
+          <Paths />
+        </div>
+        :
+        <WORLDIDwidget />
+      }
     </>
   )
 }
--- client/src/components/Live/AdvertisementForm.js
+++ client/src/components/Live/AdvertisementForm.js
@@ -3,23 +3,49 @@ import { useState } from "react";
 import Button from "../UI/Button";
 import Input from "../UI/Input";
 
+import classes from './AdvertisementForm.module.css';
+
 const AdvertisementForm = () => {
+    const [formInput, setFormInput] = useState({
+        name: '',
+        url: '',
+        amount: ''
+    });
     const [isFormVisible, setIsFormVisible] = useState(false);
 
     const showOrHideFormHandler = () =>{
         setIsFormVisible(!isFormVisible);
     }
 
-    const inputHandler = () =>{
-
+    const inputHandler = (label, inputValue) =>{
+        switch(label){
+            case 'Name':
+                setFormInput((prevState)=>{
+                    return {...prevState, name: inputValue}
+                });
+                break;
+            case 'Image Url':
+                setFormInput((prevState)=>{
+                    return {...prevState, url: inputValue}
+                });
+                break;
+            case 'Amount':
+                setFormInput((prevState)=>{
+                    return {...prevState, amount: inputValue}
+                });
+                break;
+            default:
+                break;
+        }
     }
-    const formSubmitHandler = () =>{
-        
+    const formSubmitHandler = (event) =>{
+        event.preventDefault();
+        console.log('Data is: ', formInput.name, formInput.url, formInput.amount);
     }
 
     return <div>
             <Button classes = {`btn-accent btn-outline`} onClick = {showOrHideFormHandler}>{isFormVisible ? "Hide Advertisement Form" : "Show Advertisement Form"}</Button>
-            {isFormVisible && <div>
+            {isFormVisible && <div className= {classes.ad}>
                 <form onSubmit = {formSubmitHandler}>
                 <Input 
                     type = 'text' 
@@ -36,10 +62,11 @@ const AdvertisementForm = () => {
                 <Input
                     type = 'number'
                     label = 'Amount'
+                    min = '1'
                     placeholder = 'Amount you want to pay to run ad'
                     inputChange = {inputHandler}
                 />
-                <Button classes = 'btn-warning'>Click to add advertisement</Button>
+                <Button classes = {`btn-warning ${classes.submit}`}>Click to add advertisement</Button>
             </form>
             <p>*The amount you pay decides for how  <br /> long your advertisement will run. <br/> 
             For example - If you pay 1 MATIC and <br/> 10 people are watching your stream <br /> then your advertisement will run for 10 seconds. 
