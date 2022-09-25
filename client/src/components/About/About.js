b7cbda7a95e5deb76e2a032f5e0e67ace3778a32
Bhakti <76544741+Bhakti087@users.noreply.github.com>
2022-09-26 01:31:43 +0530

    youtube link updated

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -9,7 +9,7 @@ const About = () => {
         <iframe
           width='700'
           height='400'
-          src='https://www.youtube.com/embed/LMqrUIjJbiY'
+          src='https://www.youtube.com/watch?v=S7E4hL9lU64'
           title='YouTube video player'
           frameborder='0'
           allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
